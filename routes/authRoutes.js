0c12d70446d1ff03a5dbf940d8325cae4e6d0b8a
iamirinad <irka_dimcheva@abv.bg>
2023-03-02 21:46:24 +0200

    update dev, prod variables, tweak google strategy settings

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,7 +5,9 @@ module.exports = (app) => {
     scope: ['profile', 'email']
   }));
 
-  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
+    res.redirect('/surveys');
+  });
 
   app.get('/api/logout', (req, res) => {
     req.logout();
